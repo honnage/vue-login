62e50cf89ba7b19ec9970a5c9d95394d72486156
unknown <hon_da-1998@hotmail.com>
2021-09-11 15:37:32 +0700

    Sign up

---
+++ src/components/SignUp.vue
@@ -0,0 +1,58 @@
+<template>
+    <h1>Sign Up</h1>
+    <div class="register">
+        <input type="text" v-model="username" placeholder="Enter Username">
+        <input type="text" v-model="email" placeholder="Enter Email">
+        <input type="text" v-model="password" placeholder="Enter Password">
+        <button v-on:click="signUp">Sign Up</button>
+    </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+   name:"SingUp",
+   data(){
+       return{
+           username:'',
+           email:'',
+           password:'',
+       }
+   },
+   methods:{
+        async signUp(){
+           let result = await axios.post("http://localhost:3000/users",{
+               username:this.username,
+               email:this.email,
+               password:this.password,
+           });
+           
+           console.warn(result);
+           if(result.status == 201){
+               localStorage.setItem("user-info", JSON.stringify(result.data))
+               this.$router.push({name:"Home"})
+           }
+       }
+   }
+}
+</script>
+
+<style>
+.register input, .login input, .add input {
+  width: 300px;
+  height: 40px;
+  padding-left: 20px;
+  display: block;
+  margin-bottom: 30px;
+  margin-right: auto;
+  margin-left: auto;
+  border: 1px solid skyblue;
+}
+.register button, .login button, .add button {
+  width: 320px;
+  height: 40px;
+  border: 1px solid skyblue;
+  background: skyblue;
+  color: #fff;
+  cursor: pointer;
+}
+</style>
