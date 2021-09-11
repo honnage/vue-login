98f294d4e1ab72e5c9c3a82a586743252c9f319f
unknown <hon_da-1998@hotmail.com>
2021-09-11 16:42:59 +0700

    Complete Sign Page and Redirections

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Hello {{ name }} User, Welcome on Home Page</h1>
+</template>
+<script>
+// import axios from "axios";
+export default {
+  name: "Home",
+  mounted() {
+    let user = localStorage.getItem("user-info");
+    if (!user) {
+      this.$router.push({ name: "SignUp" });
+    }
+  },
+};
+</script>
+<style>
+td {
+  width: 160px;
+  height: 40px;
+}
+</style>
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,43 +1,50 @@
 <template>
-    <h1>Sign Up</h1>
-    <div class="register">
-        <input type="text" v-model="username" placeholder="Enter Username">
-        <input type="text" v-model="email" placeholder="Enter Email">
-        <input type="text" v-model="password" placeholder="Enter Password">
-        <button v-on:click="signUp">Sign Up</button>
-    </div>
+  <h1>Sign Up</h1>
+  <div class="register">
+    <input type="text" v-model="username" placeholder="Enter Username" />
+    <input type="text" v-model="email" placeholder="Enter Email" />
+    <input type="text" v-model="password" placeholder="Enter Password" />
+    <button v-on:click="signUp">Sign Up</button>
+  </div>
 </template>
 <script>
-import axios from 'axios'
+import axios from "axios";
 export default {
-   name:"SingUp",
-   data(){
-       return{
-           username:'',
-           email:'',
-           password:'',
-       }
-   },
-   methods:{
-        async signUp(){
-           let result = await axios.post("http://localhost:3000/users",{
-               username:this.username,
-               email:this.email,
-               password:this.password,
-           });
-           
-           console.warn(result);
-           if(result.status == 201){
-               localStorage.setItem("user-info", JSON.stringify(result.data))
-               this.$router.push({name:"Home"})
-           }
-       }
-   }
-}
+  name: "SingUp",
+  data() {
+    return {
+      username: "",
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    async signUp() {
+      let result = await axios.post("http://localhost:3000/users", {
+        username: this.username,
+        email: this.email,
+        password: this.password,
+      });
+
+      console.warn(result);
+      if (result.status == 201) {
+        localStorage.setItem("user-info", JSON.stringify(result.data));
+        this.$router.push({ name: "Home" });
+      }
+    },
+  },
+  mounted() {
+    let user = localStorage.getItem("user-info");
+    if (user) {
+      this.$router.push({ name: "Home" });
+    }
+  },
+};
 </script>
 
 <style>
-.register input, .login input, .add input {
+.register input,
+.login input {
   width: 300px;
   height: 40px;
   padding-left: 20px;
@@ -47,7 +54,8 @@ export default {
   margin-left: auto;
   border: 1px solid skyblue;
 }
-.register button, .login button, .add button {
+.register button,
+.login button {
   width: 320px;
   height: 40px;
   border: 1px solid skyblue;
