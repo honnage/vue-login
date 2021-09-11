9c8c1108622b53e31f6368b676c1a8a2b9255d98
unknown <hon_da-1998@hotmail.com>
2021-09-11 23:30:28 +0700

    Alert result status login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,152 +1,175 @@
 <template>
-
   <!-- <div class="container"> -->
-    <div class="login">
-        <h1>Login</h1>
-        <!-- <input type="text" model="username" placeholder="Enter username" />
-        <input type="text" model="password" placeholder="Ente Password" />
-        <button v-on:click="login">Login</button> -->
+  <div class="login">
+    <h1>Login</h1>
 
-        <form method="post">
-            <div class="txt_field">
-                <input type="text" v-model="userName" placeholder="Enter Username" />
- 
-                <!-- <input type="text" required> -->
-                <span></span>
-                <label>Username</label>
-            </div>
-            <div class="txt_field">
-                <input type="text" v-model="passWord" placeholder="Enter Password" />
-
-                <!-- <input type="pasword" required> -->
-                 <span></span>
-                <label>Pasword</label>
-            </div>
-            <div class="pass">Forgot Password</div>
-            <input type="submit" value="Login">
-            <div class="signup_link">Not a member <a href="">Signup</a></div>
-        </form>
-    <!-- </div> -->
+    <div v-if="error" class="alert alert-danger" role="alert">
+      {{ error }}
+    </div>
+    <data>
+      <div class="txt_field">
+        <input type="text" v-model="userName" required />
+        <span></span>
+        <label>Username</label>
+      </div>
+      <div class="txt_field">
+        <input type="password" v-model="passWord" required />
+        <span></span>
+        <label>Pasword</label>
+      </div>
+      <div class="pass">Forgot Password</div>
+      <input type="submit" v-on:click="Login" value="Login" />
+      <div class="signup_link">Not a member <a href="">Signup</a></div>
+    </data>
   </div>
-
 </template>
 <script>
+import axios from "axios";
 export default {
   name: "Login",
   data() {
     return {
       userName: "",
       passWord: "",
+      error: "",
     };
+  },
+  methods: {
+    async Login() {
+      //   console.warn(this.userName, this.passWord);
+
+      let result = await axios.post(
+        `http://api.teedev.online/api/v1/user/login`,
+        {
+          userName: this.userName,
+          passWord: this.passWord,
+        }
+      );
+      console.warn(result.data);
+
+      if (result.data.success == true) {
+        // localStorage.setItem("user-info", JSON.stringify(result.data[0]));
+        // this.$router.push({ name: "Home" });
+      } else {
+        console.warn(result.data.errorCode);
+        this.error = result.data.errorText;
+      }
+    },
+  },
+  mounted() {
+    let user = localStorage.getItem("user-info");
+    if (user) {
+      this.$router.push({ name: "Home" });
+    }
   },
 };
 </script>
 
 <style>
-body{
+body {
   margin: 0;
   padding: 0;
   background: linear-gradient(120deg, #eef8ff, #afd1fd);
   height: 100vh;
   overflow: hidden;
 }
-.login{
+.login {
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  width: 400px;
+  width: 600px;
   background: white;
-  border-radius: 10px;
+  border-radius: 25px;
 }
-.login h1{
-    text-align: center;
-    margin-top: 10px;
-    padding: 0 0 20px 0;
-    border-bottom: 1px solid silver;
+.login h1 {
+  text-align: center;
+  margin-top: 25px;
+  padding: 0 0 20px 0;
+  border-bottom: 1px solid silver;
 }
-.login form{
-    padding: 0 40px;
-    box-shadow: border-box;
+.login data {
+  /* padding: 0 5px; */
+  box-shadow: border-box;
 }
-form .txt_field{
-    position: relative;
-    border-bottom: 2px solid #adadad;
-    margin: 30px 0;
+data .txt_field {
+  position: relative;
+  border-bottom: 2px solid #adadad;
+  margin: 30px 0;
 }
-.txt_field input{
-    width: 100%;
-    padding: 0 5px;
-    height: 40px;
-    font-size: 16px;
-    border: none;
-    background: none;
-    outline: none;
+.txt_field input {
+  width: 100%;
+  padding: 0 5px;
+  height: 40px;
+  font-size: 16px;
+  border: none;
+  background: none;
+  outline: none;
 }
-.txt_field label{
-    position: absolute;
-    top: 50%;
-    left: 5px;
-    color: #adadad;
-    transform: translateY(-50%);
-    font-size: 16px;
-    pointer-events: none;
-    transition: .5s;
+.txt_field label {
+  position: absolute;
+  top: 50%;
+  left: 60px;
+  color: #adadad;
+  transform: translateY(-50%);
+  font-size: 16px;
+  pointer-events: none;
+  transition: 0.5s;
 }
-.txt_field span::before{
-    content: '';
-    position: absolute;
-    top: 40px;
-    left: 0;
-    width: 0%;
-    height: 2px;
-    background: #2691d9;
-    transition: .5s;
+.txt_field span::before {
+  content: "";
+  position: absolute;
+  top: 40px;
+  left: 0;
+  width: 0%;
+  height: 2px;
+  background: #2691d9;
+  transition: 0.5s;
 }
 .txt_field input:focus ~ label,
-.txt_field input:valid ~ label{
-    top: -5px;
-    color: #2691d9;
+.txt_field input:valid ~ label {
+  top: -5px;
+  color: #2691d9;
 }
 .txt_field input:focus ~ span::before,
-.txt_field input:valid ~ span::before{
-   width: 100%;
+.txt_field input:valid ~ span::before {
+  width: 100%;
 }
-.pass{
-    margin: -5px 0 20px 5px;
-    color: #a6a6a6;
-    cursor: pointer;
+.pass {
+  margin: -5px 0 20px 5px;
+  color: #a6a6a6;
+  cursor: pointer;
 }
-.pass:hover{
-    text-decoration: underline;
+.pass:hover {
+  text-decoration: underline;
 }
-input[type="submit"]{
-    width: 100%;
-    height: 50px;
-    border: 1px solid;
-    background: #2691d9;
-    border-radius: 25px;
-    font-size: 18px;
-    color: #e9f4fb;
-    font-weight: 700;
-    cursor: pointer;
-    outline: none;
+input[type="submit"] {
+  width: 80%;
+  height: 50px;
+  border: 1px solid;
+  background: #2691d9;
+  border-radius: 25px;
+  font-size: 18px;
+  color: #e9f4fb;
+  font-weight: 700;
+  cursor: pointer;
+  outline: none;
 }
-input[type="submit"]:hover{
-    border-color: #2691d9;
-    transition: .5s;
+input[type="submit"]:hover {
+  border-color: #2691d9;
+  transition: 0.5s;
 }
-.signup_link{
-    margin: 30px 0;
-    text-align: center;
-    font-size: 16px;
-    color: #666666;
+.signup_link {
+  margin: 30px 0;
+  text-align: center;
+  font-size: 16px;
+  color: #666666;
 }
-.signup_link a{
-    color: #2691d9;
-    text-decoration: none;
+.signup_link a {
+  color: #2691d9;
+  text-decoration: none;
 }
-.signup_link a:hover{
-    text-decoration: underline;
+.signup_link a:hover {
+  text-decoration: underline;
 }
 </style>
